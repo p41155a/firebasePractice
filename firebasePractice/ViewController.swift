f83e2416adbbc80aee4c25ef41e5e191bc204ad5
p41155a <p41155a@gmail.com>
2019-09-27 21:25:18 +0900

    Initial Commit

---
+++ firebasePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebasePractice
+//
+//  Created by MC975-107 on 27/09/2019.
+//  Copyright © 2019 comso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
